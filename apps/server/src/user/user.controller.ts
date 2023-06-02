127deb0a018f23c07ced7f4858dced2d840fc24f
ErickCelestino <erickcelestimo@gmail.com>
2023-06-01 23:34:59 -0300

    Fake create user and more infomation

---
+++ apps/server/src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return this.userService.createUser(createUser);
+  }
+}
