6b3d7c6667f14ba5397d827d5bb812378040150d
ErickCelestino <erickcelestimo@gmail.com>
2023-06-01 23:55:17 -0300

    Create users and cryptography password

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
+  async createUser(@Body() createUser: CreateUserDto): Promise<User> {
     return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUser(): Promise<User[]> {
+    return this.userService.getAllUser();
   }
 }
